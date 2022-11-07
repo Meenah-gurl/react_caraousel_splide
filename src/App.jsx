2b7f85f3aab3d7ee74988081a9d54f1855e7b1c9
meenah-gurl <aminasani.as64@gmail.com>
2022-11-07 08:42:23 +0100

    Completed project

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,21 @@
 import React from 'react'
-import { Splide, SplideSlide } from '@splidejs/react-splide'
-import { AutoScroll } from '@splidejs/splide-extension-auto-scroll'
-import "@splidejs/splide/dist/css/splide.min.css";
+import Slider from './components/Slider';
 
-export const App = () => {
+
+export function App  ()  {
   return (
-    <div className='max-w-2xl mx-auto py-10 space-y-3'>
-      <div className='text-center text-lg'>Building Slider with Splide js Library</div>
-      <div className='py-10 bg-purple-500 flex justify-center items-center rounded-lg'>
-        <Splide options={{
-          type: 'loop',
-          rewind:true, 
-          autoplay: true,
-          perMove: 1,
-          perPage: 2, 
-          gap: '2rem',
-          arrows: false,
-          pagination: false,
-          autoScroll: {
-            pauseOnHover: true,
-            pauseOnFocus: false,
-            speed: 2
-          },
-        }}
-        extensions={{ AutoScroll }}>
-        <SplideSlide>
-          <div className=' h-72 flex justify-center items-center'>
-             <img src="/src/assets/nature1.jpg" className='w-96 h-full' alt="" />
-          </div>
-        </SplideSlide>
-        <SplideSlide>
-          <div className=' h-72 flex justify-center items-center '>
-              <img src="/src/assets/nature2.jpg" className='w-96 h-full' alt="" />
-          </div>
-        </SplideSlide>
-        <SplideSlide>
-          <div className=' h-72 flex justify-center items-center '>
-              <img src="/src/assets/nature3.jpg" className='w-96 h-full' alt="" />
-          </div>
-        </SplideSlide>
-        </Splide>
+    // <Routes>
+    //   <Route path='/' element={<Home />} />
+    // </Routes>
+   
+    <div className='max-w-2xl mx-auto py-10 flex justify-center items-center space-y-3 flex-col'>
+        <div className='text-2xl font-bold text-gray-800'>Building Carousel with Splide.js</div>
+        <div className=" bg-purple-600  rounded-lg py-10">
+            <Slider 
+                imageUrl="/src/assets/nature1.jpg " 
+            />
+        </div>
       </div>
-    </div>
-
   )
 }
 
